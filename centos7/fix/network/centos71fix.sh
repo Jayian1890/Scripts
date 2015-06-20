976a8708bbeb9d4fe930a2704c0334e7ea4621ed
Tsume <jaredt741@gmail.com>
2015-06-20 15:29:53 -0500

    no message

--- centos7/fix/network/centos71fix.sh
+++ centos7/fix/network/centos71fix.sh
@@ -2,6 +2,5 @@
 #CentOS 7.1 Network Fix for OpenVZ
 ifup venet0 && ifup venet0:0
 rpm -iUvh --force "https://github.com/Tsume/Scripts/raw/master/centos7/fix/network/initscripts-9.49.17-1.el7.x86_64.rpm"
-sed -i.bak "s/IPADDR=.*/IPADDR=127.0.0.1/" "/etc/sysconfig/network-scripts/ifcfg-venet0"
 service network restart
 echo "Complete"
