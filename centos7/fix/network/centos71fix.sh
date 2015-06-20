92db22d77e81825454f0978efac4f190287b24cb
Tsume <jaredt741@gmail.com>
2015-06-20 15:14:28 -0500

    no message

--- centos7/fix/network/centos71fix.sh
+++ centos7/fix/network/centos71fix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #CentOS 7.1 Network Fix for OpenVZ
-rpm -iUvh --force "https://github.com/Tsume/Scripts/raw/master/initscripts-9.49.17-1.el7.x86_64.rpm"
+rpm -iUvh --force "https://github.com/Tsume/Scripts/raw/master/centos7/fix/network/initscripts-9.49.17-1.el7.x86_64.rpm"
 sed -i.bak "s/IPADDR=.*/IPADDR=127.0.0.1/" "/etc/sysconfig/network-scripts/ifcfg-venet0"
 service network restart
 echo "Complete"
