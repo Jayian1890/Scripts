d95ce37a6ef21a6c43dfc02df4d4a1f91804d8cc
Tsume <jaredt741@gmail.com>
2015-06-20 18:54:24 -0500

    no message

--- centos7/fix/network/centos71fix.sh
+++ centos7/fix/network/centos71fix.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #CentOS 7.1 Network Fix for OpenVZ
-ifup venet0 && ifup venet0:0
 rpm -iUvh --force "https://github.com/Tsume/Scripts/raw/master/centos7/fix/network/initscripts-9.49.17-1.el7.x86_64.rpm"
 service network restart
 echo "Complete"
