f9f875cc9902636c1095be9c8edff13f36f34c53
Tsume <jaredt741@gmail.com>
2015-06-20 19:28:22 -0500

    no message

--- centos7/fix/network/centos71fix.sh
+++ centos7/fix/network/centos71fix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #CentOS 7.1 Network Fix for OpenVZ
+ifup venet0:0
 sed -i.bak "264s/.*/\/sbin\/arping -q -c 2 -w \$\{ARPING_WAIT:-3\} -D -I \$\{parent_device\} \$\{IPADDR\}\nif [ $? = 1 ]; then/g" "/etc/sysconfig/network-scripts/ifup-aliases"
 service network restart
 echo "Complete"
