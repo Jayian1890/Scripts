dfdb74baa976511d5c004ab398d5cebf50130491
Tsume <jaredt741@gmail.com>
2015-10-15 06:48:43 -0500

    no message

--- search.sh
+++ search.sh
@@ -1,12 +1,12 @@
-#!/sh/bin
+#!/bin/bash
 IP_ADDRESS=$1
 
 if [ -z "$1" ]
   then
     echo "No argument supplied"
   else
-	cd /tmp
-	vzlist -o hostname,ctid,ip >> vzlist
-	grep "$IP_ADDRESS" vzlist
+    cd /tmp
+    vzlist -o hostname,ctid,ip >> vzlist
+    grep "$IP_ADDRESS" vzlist
 fi
 
