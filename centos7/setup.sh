08760e5511333f9c080a8164a3b77c4ac82c096d
Jared T <637445+Jayark365@users.noreply.github.com>
2021-11-19 05:29:14 -0600

    Update setup.sh

--- centos7/setup.sh
+++ centos7/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 SSH_DIR=~/.ssh
 SSH_KEYS=authorized_keys
-SSH_PORT=22
 PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAykZ1FVy8AJLv52aKZnMbPE2S9tcHi37/Rc1Z6fRhr++3PR9OZvNubLRU2iGObjD15LHuSI+m7Na0ZAkQMd7F/it4WhA9tTZyW0BEZGOhSIeXp+e3JLzt9DHBcwIG0ZEYqCFIdVjxoT0BzmUmmwmu2ZG8t07WE9m3W30sFOCp3f0= rsa-key-20130113"
 
 echo "Adding ssh key to authorized_keys..."
@@ -17,9 +16,6 @@ echo "Setting permissions..."
 chmod 700 $SSH_DIR
 chmod 600 "$SSH_DIR/$SSH_KEYS"
 
-echo "Changing ssh port to $SSH_PORT"
-sed -i.bak "s/#Port 22/Port $SSH_PORT/g" "/etc/ssh/sshd_config"
-
 echo "Disabling password logins..."
 sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" "/etc/ssh/sshd_config"
 
@@ -28,8 +24,3 @@ service sshd restart
 
 echo "Updating system..."
 yum install epel-release -y && yum install htop -y && yum update -y
-
-echo "Adding firewalld exceptions..."
-firewall-cmd --permanent --zone=public --add-port=$SSH_PORT/tcp
-firewall-cmd --reload
-
