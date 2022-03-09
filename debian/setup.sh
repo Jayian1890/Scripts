b59071f39b78f33317fa3ec32e7968c01baf069d
Jared T <637445+Jayark365@users.noreply.github.com>
2022-03-08 22:34:15 -0500

    Update setup.sh

--- debian/setup.sh
+++ debian/setup.sh
@@ -5,7 +5,7 @@ SSH_PORT=22
 PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAykZ1FVy8AJLv52aKZnMbPE2S9tcHi37/Rc1Z6fRhr++3PR9OZvNubLRU2iGObjD15LHuSI+m7Na0ZAkQMd7F/it4WhA9tTZyW0BEZGOhSIeXp+e3JLzt9DHBcwIG0ZEYqCFIdVjxoT0BzmUmmwmu2ZG8t07WE9m3W30sFOCp3f0= rsa-key-20130113"
 
 echo "Updating system..."
-apt-get update && apt-get upgrade -y && apt-get install htop -y
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install htop -y
 
 echo "Adding ssh key to authorized_keys..."
 if [ ! -d "$SSH_DIR" ]; then
