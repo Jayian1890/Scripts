cfdc4d388f246d172a444b3e8c39f52a6de001ec
Jay <jaredt741@gmail.com>
2024-08-19 05:37:33 -0500

    Update setup.sh

--- debian/setup.sh
+++ debian/setup.sh
@@ -2,10 +2,13 @@
 SSH_DIR=~/.ssh
 SSH_KEYS=authorized_keys
 SSH_PORT=22
-PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAIEAykZ1FVy8AJLv52aKZnMbPE2S9tcHi37/Rc1Z6fRhr++3PR9OZvNubLRU2iGObjD15LHuSI+m7Na0ZAkQMd7F/it4WhA9tTZyW0BEZGOhSIeXp+e3JLzt9DHBcwIG0ZEYqCFIdVjxoT0BzmUmmwmu2ZG8t07WE9m3W30sFOCp3f0= rsa-key-20130113"
+PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDn7fiB5uKn1/FXfsGKE7Ov/xELksI7srTyFJ4ncjTdw6HhEpXEsAXVtR51BT+GzOFFrl5nwyNtsC8wGTviGUO/srWuWHRiol6xZ9K0+7jKK/53bXoiZ1CCb0HYJ2FN759uLmn4kX2nm/cvbBNPpyGUT0tWoVKMNfkRfdReFh08zpeCe8m+uBYuUEMaItIhg+uUzdH3IK3EhTpy53xqhn2XVLKzo77f3c1kpsupyiBCm5g5nnLjajt2B4bqHexwMQXXj/HhPYvsT5aMgVEY9ta5P/tjaZ90FEi4QkIQl+2a074ozHcUJ7UFDVcNVbDdZUaPV6gfVQ19NtTLV7v1gwT79xCzAKR4+CY3BK0MS2dGfOhktC+FAa19l5THnskGNmQ0hyDdVvlGxQwIN8tx2TgHzhiKEM8kq0wil/Bb14ikC3fd5W9jG3XqUVRysfDClkIvrnf0i8CbYS1gsoDPYPsdgVv80cXTjtb3W8LX4go7zYoGGp8DVJVtZQxD20t3VEM= jared@interlacedpixel.com"
 
 echo "Updating system..."
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install htop -y
+sudo apt-get update && sudo apt-get upgrade -y
+
+echo "Installing base packages..."
+sudo apt-get install curl systemd-resolved cifs-utils htop -y
 
 echo "Adding ssh key to authorized_keys..."
 if [ ! -d "$SSH_DIR" ]; then
@@ -20,6 +23,28 @@ echo "Setting permissions..."
 sudo chmod 700 $SSH_DIR
 sudo chmod 600 "$SSH_DIR/$SSH_KEYS"
 
-#echo "Creating user 'jared'"
-#sudo adduser jared
-#echo "jared  ALL=(ALL:ALL) ALL" | sudo tee --append /etc/sudoers
+echo "Linking stub-resolv config file..."
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+echo "Setting name servers..."
+sudo resolvectl dns ens192 1.1.1.1
+
+echo "Restarting systemd-resolved..."
+sudo systemctl restart systemd-resolved
+
+echo "Installing Tailscale package... (Interaction required)"
+curl -fsSL https://tailscale.com/install.sh | sh
+
+echo "Starting Tailscale service..."
+sudo tailscale up
+
+echo "Setting up Samba client..."
+sudo echo 'user=jayian' | sudo tee /.smbcredentials
+sudo echo 'password=Admins!@!301' | sudo tee -a /.smbcredentials
+sudo echo "//MediaServer/media /media cifs credentials=/.smbcredentials,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0" | sudo tee -a /etc/fstab
+sudo systemctl daemon-reload 
+
+echo "Mounting samba NFS to /media"
+sudo mount -a
+
+echo "Everything has been completed!!! Hopefully without error"
